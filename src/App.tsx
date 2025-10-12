66270813b6a281d6280c5fa3e0174edd8afdb0ac
Aakash  Raina <aakashraina9@gmail.com>
2025-10-12 13:03:06 +0530

    Add card display section to App component

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ import FlightIcon from "./assets/Flights.svg";
 import FileStorageIcon from "@/assets/File_storage.svg";
 import MegaPhoneIcon from "@/assets/Megaphone.svg";
 import { ChevronRight } from "lucide-react";
+import VisaLogo from "@/assets/Visa_Logo.svg";
+import AspireLogo from "@/assets/Aspire_Logo_Card.svg";
+import ShowCardNumberIcon from "@/assets/Eye_card.svg";
 
 const recentTransactions = [
   {
@@ -69,27 +72,109 @@ export function formatDate(timestamp: number): string {
 function App() {
   return (
     <Layout>
-      <div className='flex flex-col md:flex-row border-zinc-50 border-2 h-full rounded gap-10 p-10 mx-4'>
-        <div className='flex-1 md:flex-1 border-2 border-red-300'>
-          <Carousel className='border-2 border-blue-300'>
-            <CarouselContent>
-              <CarouselItem>
-                <div className='h-full bg-green-500 border-amber-300 border-2 rounded-lg'>
-                  <h2 className='text-xl font-semibold mb-4'>Left Content</h2>
-                  <div className='space-y-4'>
-                    <div className='bg-muted p-4 rounded-md'>
-                      <h3 className='font-medium mb-2'>Dashboard Overview</h3>
-                      <p className='text-sm text-muted-foreground'>
-                        Overview of your application metrics and key performance
-                        indicators.
-                      </p>
+      <div className='flex flex-col md:flex-row h-full rounded gap-10 md:p-10 mx-4'>
+        <div className='flex-1 md:flex-1'>
+          <div className='relative'>
+            <Carousel className='mt-5'>
+              <CarouselContent>
+                <CarouselItem>
+                  <div className='h-full bg-green-500 rounded-lg flex flex-col'>
+                    <div className='flex-1 flex justify-end items-center p-4'>
+                      <div className='text-white font-semibold'>
+                        <img src={AspireLogo} alt='Aspire Logo' />
+                      </div>
+                    </div>
+
+                    <div className='flex-1 flex items-center p-4'>
+                      <div>
+                        <p className='text-white text-2xl font-bold'>
+                          Mark Henry
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className='flex-1 flex items-center p-4'>
+                      <div className='text-white font-medium'>
+                        <p className='font-semibold text-2xl inline-block'>
+                          •••• •••• ••••
+                        </p>{" "}
+                        <p className='font-medium text-xl inline-block'>1234</p>
+                      </div>
+                    </div>
+
+                    <div className='flex-1 flex items-center gap-8 p-4'>
+                      <div>
+                        <p className='text-white text-sm font-bold'>
+                          Thru: 12/28
+                        </p>
+                      </div>
+                      <div>
+                        <p className='text-white text-sm font-bold'>CVV: 123</p>
+                      </div>
+                    </div>
+
+                    <div className='flex-1 flex items-center p-4 justify-end'>
+                      <div className='text-white text-sm'>
+                        <img src={VisaLogo} alt='Visa Logo' />
+                      </div>
                     </div>
                   </div>
-                </div>
-              </CarouselItem>
-            </CarouselContent>
-          </Carousel>
-          <div className='hidden md:block'>
+                </CarouselItem>
+                <CarouselItem>
+                  <div className='h-full bg-green-500 rounded-lg flex flex-col'>
+                    <div className='flex-1 flex justify-end items-center p-4'>
+                      <div className='text-white font-semibold'>
+                        <img src={AspireLogo} alt='Aspire Logo' />
+                      </div>
+                    </div>
+
+                    <div className='flex-1 flex items-center p-4'>
+                      <div>
+                        <p className='text-white text-2xl font-bold'>
+                          Mark Henry
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className='flex-1 flex items-center p-4'>
+                      <div className='text-white font-medium'>
+                        <p className='font-semibold text-2xl inline-block'>
+                          •••• •••• ••••
+                        </p>{" "}
+                        <p className='font-medium text-xl inline-block'>1234</p>
+                      </div>
+                    </div>
+
+                    <div className='flex-1 flex items-center gap-8 p-4'>
+                      <div>
+                        <p className='text-white text-sm font-bold'>
+                          Thru: 12/28
+                        </p>
+                      </div>
+                      <div>
+                        <p className='text-white text-sm font-bold'>CVV: 123</p>
+                      </div>
+                    </div>
+
+                    <div className='flex-1 flex items-center p-4 justify-end'>
+                      <div className='text-white text-sm'>
+                        <img src={VisaLogo} alt='Visa Logo' />
+                      </div>
+                    </div>
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+            </Carousel>
+            <div className='absolute -top-7 right-0 text-sm text-green-500 font-bold cursor-pointer'>
+              <img
+                src={ShowCardNumberIcon}
+                alt='Show card number'
+                className='inline'
+              />{" "}
+              <p className='inline'> Show card number</p>
+            </div>
+          </div>
+          <div className='hidden md:block mt-5'>
             <CardOptions />
           </div>
         </div>
@@ -102,67 +187,7 @@ function App() {
                   <p className='text-sky-950'>Card details</p>
                 </div>
               </AccordionTrigger>
-              <AccordionContent>
-                <div>
-                  <div>
-                    {recentTransactions.map((transaction, index) => (
-                      <div
-                        className='p-4 flex border-b-2 border-l-1 border-r-1 border-[#F0F0F0]'
-                        key={index}
-                      >
-                        <div className='flex-[1] flex flex-col justify-center items-center'>
-                          <div
-                            className='flex w-8 h-8 rounded-full justify-center items-center mb-2'
-                            style={{ backgroundColor: transaction.color }}
-                          >
-                            <img
-                              className='w-4 h-4'
-                              src={transaction.icon}
-                              alt={transaction.title}
-                            />
-                          </div>
-                        </div>
-                        <div className='flex-[9] flex flex-row pl-4'>
-                          <div className='flex-1 flex flex-col'>
-                            <p className='text-neutral-800 mb-2 text-sm font-semibold inline-block'>
-                              {transaction.title}
-                            </p>
-                            <p className='text-neutral-400 text-xs mb-2'>
-                              {formatDate(transaction.createdAt)}
-                            </p>
-                            <p className='text-xs text-blue-700'>
-                              {transaction.type === "debit"
-                                ? "Charged to debit card"
-                                : "Refund on debit card"}
-                            </p>
-                          </div>
-                          <div className='flex-1 flex flex-row justify-end items-start gap-2'>
-                            <p
-                              className={`mb-2 text-sm font-semibold inline-block ${
-                                transaction.type === "credit"
-                                  ? "text-green-500"
-                                  : "text-neutral-800"
-                              }`}
-                            >
-                              {transaction.type === "credit" ? "+" : "-"} S${" "}
-                              {transaction.amount}
-                            </p>
-                            <ChevronRight
-                              size={16}
-                              className='mt-1 text-neutral-400'
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    ))}
-                    <div className='bg-emerald-50 flex justify-center items-center p-4 rounded-b-md'>
-                      <p className='text-sm text-green-500'>
-                        View all card transactions
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </AccordionContent>
+              <AccordionContent></AccordionContent>
             </AccordionItem>
           </Accordion>
           <Accordion type='single' collapsible className='my-3'>
@@ -207,7 +232,7 @@ function App() {
                                 : "Refund on debit card"}
                             </p>
                           </div>
-                          <div className='flex-1 flex flex-row justify-end items-start gap-2'>
+                          <div className='flex-1 flex flex-row justify-end items-start'>
                             <p
                               className={`mb-2 text-sm font-semibold inline-block ${
                                 transaction.type === "credit"
@@ -220,7 +245,7 @@ function App() {
                             </p>
                             <ChevronRight
                               size={16}
-                              className='mt-1 text-neutral-400'
+                              className='mb-0.5 text-neutral-400'
                             />
                           </div>
                         </div>
